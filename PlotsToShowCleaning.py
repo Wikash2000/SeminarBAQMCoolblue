229e53755d6aa1a067ecefe2bae24a88426a0b3e
Nic2242 <531725ns@eur.nl>
2025-03-22 17:56:45 +0100

    Update PlotsToShowCleaning.py

--- PlotsToShowCleaning.py
+++ PlotsToShowCleaning.py
@@ -1,3 +1,9 @@
+"""
+Created on Sat Mar 22 17:50:42 2025
+
+@author: nicho
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -12,11 +18,8 @@ Key Step:
    - Plots both time series to visualize the impact of removing outliers.
 """
 
-
-
-
 # Load the dataset
-data = pd.read_csv('web_data_with_product_types.csv')
+data = pd.read_csv('web_data_with_product_types')
 #data = pd.read_csv('web_data_outliers_removed.csv')
 #data = pd.read_csv('web_data_outlier_removed_incl_neg_peaks.csv')
 
@@ -34,7 +37,7 @@ grand_total_visits = total_web_visits + total_app_visits
 # Figure 1: Plot Total Web Visits (Original vs Outliers Removed)
 # ------------------------------------
 # Group by datetime and sum web visits for both datasets
-data_outliers_removed = pd.read_csv('web_data_outlier_removed_combined.csv')
+data_outliers_removed = pd.read_csv('web_data_outlier_removed.csv')
 total_visits_original = data.groupby('datetime')['visits_web'].sum()
 total_visits_outliers_removed = data_outliers_removed.groupby('datetime')['visits_web'].sum()
 
@@ -55,6 +58,7 @@ plt.xlabel('Datetime')
 plt.ylabel('Total Web Visits')
 plt.title('Comparison of Total Web Visits (Original vs Outliers Removed)')
 plt.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+plt.xlim(pd.Timestamp("2023-10-23 20:00:00"),pd.Timestamp("2023-10-23 22:00:00"))
 plt.grid(True)
 plt.tight_layout()
 
